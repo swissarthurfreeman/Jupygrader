58a9776f47bf4a311ae0e991b9e8818bd8c00c73
gordon <953-Arthur.Freeman@users.noreply.gitlab.unige.ch>
2023-08-28 13:07:44 +0200

    working scala kernel for instructor

--- jupyterhub/global_nbgrader_config.py
+++ jupyterhub/global_nbgrader_config.py
@@ -2,3 +2,4 @@ c = get_config()
 
 c.CourseDirectory.course_id = "course101"
 c.Exchange.root = "/usr/local/share/nbgrader/exchange"
+
--- jupyterhub/local_nbgrader_config.py
+++ jupyterhub/local_nbgrader_config.py
@@ -1,3 +1,9 @@
 c = get_config()
 
 c.CourseDirectory.root = '/home/grader-course101/course101'
+
+c.ClearSolutions.begin_solution_delimeter = "BEGIN SOLUTION"
+c.ClearSolutions.end_solution_delimeter = "END SOLUTION"
+c.ClearSolutions.code_stub = {
+    "scala": "// your code here\n throw new NotImplementedError()"
+}
